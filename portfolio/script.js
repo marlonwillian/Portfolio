bc71da79216f7571da184f7447642d2f7127e0de
Marlon Willian Silva Barros <mwsb2004@gmail.com>
2023-06-19 12:08:59 -0300

    inicio do modo claro/escuro

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,4 +1,10 @@
+let body = document.getElementById("body")
+
 let menu = document.getElementById("menu")
+let inicio = document.getElementById("inicio")
+let sobre = document.getElementById("sobre")
+let projetos = document.getElementById("projetos")
+
 let mobile_menu = document.getElementById("mobile_menu")
 
 function clickMenu() {
@@ -7,4 +13,11 @@ function clickMenu() {
     } else {
         menu_mobile.style.display = 'grid'
     }
+}
+
+function modoClaro() {
+    body.style.backgroundColor = 'white'
+    inicio.style.color = 'rgb(17, 38, 57)'
+    sobre.style.color = 'rgb(17, 38, 57)'
+    projetos.style.color = 'rgb(17, 38, 57)'
 }
