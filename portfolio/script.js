c99810ef2d8e2db15e27cf3ba61d2e75ba163aae
Marlon Willian Silva Barros <mwsb2004@gmail.com>
2023-06-19 21:44:39 -0300

    modo claro/escuro concluído

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,11 +1,22 @@
 let body = document.getElementById("body")
 
+let escuro = document.getElementById("escuro")
+let claro = document.getElementById("claro")
+
 let menu = document.getElementById("menu")
 let inicio = document.getElementById("inicio")
 let sobre = document.getElementById("sobre")
 let projetos = document.getElementById("projetos")
 
+let hamb = document.getElementById("hamb")
+
 let mobile_menu = document.getElementById("mobile_menu")
+let m_inicio = document.getElementById("m_inicio")
+let m_sobre = document.getElementById("m_sobre")
+let m_projetos = document.getElementById("m_projetos")
+
+let c_sociais = document.getElementById("c_redes_sociais")
+let e_sociais = document.getElementById("e_redes_sociais")
 
 function clickMenu() {
     if (menu_mobile.style.display == 'grid') {
@@ -17,7 +28,40 @@ function clickMenu() {
 
 function modoClaro() {
     body.style.backgroundColor = 'white'
+
     inicio.style.color = 'rgb(17, 38, 57)'
     sobre.style.color = 'rgb(17, 38, 57)'
     projetos.style.color = 'rgb(17, 38, 57)'
+
+    hamb.style.color = 'rgb(17, 38, 57)'
+
+    m_inicio.style.color = 'rgb(17, 38, 57)'
+    m_sobre.style.color = 'rgb(17, 38, 57)'
+    m_projetos.style.color = 'rgb(17, 38, 57)'
+
+    c_sociais.style.display = "none"
+    e_sociais.style.display = "block"
+
+    claro.style.display = "none"
+    escuro.style.display = "block"
+}
+
+function modoEscuro() {
+    body.style.backgroundColor = 'rgb(17, 38, 57)'
+
+    inicio.style.color = 'white'
+    sobre.style.color = 'white'
+    projetos.style.color = 'white'
+
+    hamb.style.color = 'white'
+
+    m_inicio.style.color = 'white'
+    m_sobre.style.color = 'white'
+    m_projetos.style.color = 'white'
+
+    c_sociais.style.display = "block"
+    e_sociais.style.display = "none"
+
+    escuro.style.display = "none"
+    claro.style.display = "block"
 }
