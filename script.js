6140dc3f26a2e6e4236fb0fbccf7459e1aaa7885
Marlon Willian Silva Barros <mwsb2004@gmail.com>
2023-07-04 21:51:01 -0300

    2 projetos adicionados

--- script.js
+++ script.js
@@ -31,6 +31,12 @@ let jv_github = document.getElementById("jv_github")
 let gc_projeto = document.getElementById("gc_projeto")
 let gc_github = document.getElementById("gc_github")
 
+let jc_projeto = document.getElementById("jc_projeto")
+let jc_github = document.getElementById("jc_github")
+
+let pn_projeto = document.getElementById("pn_projeto")
+let pn_github = document.getElementById("pn_github")
+
 let nome = document.getElementById("nome")
 let subtitulo = document.getElementById("subtitulo")
 let tecnologias = document.getElementById("tecnologias")
@@ -42,6 +48,9 @@ let projetos_titulo = document.getElementById("projetos_titulo")
 let jogo_da_velha = document.getElementById("jogo_da_velha")
 let gerador_de_cartas = document.getElementById("gerador_de_cartas")
 
+let jogo_da_cobrinha = document.getElementById("jogo_da_cobrinha")
+let pagina_netflix = document.getElementById("pagina_netflix")
+
 let hr1 = document.getElementById("linha_1")
 let hr2 = document.getElementById("linha_2")
 
@@ -49,6 +58,10 @@ let hr3 = document.getElementById("linha_3")
 let hr4 = document.getElementById("linha_4")
 let hr5 = document.getElementById("linha_5")
 let hr6 = document.getElementById("linha_6")
+let hr7 = document.getElementById("linha_7")
+let hr8 = document.getElementById("linha_8")
+let hr9 = document.getElementById("linha_9")
+let hr10 = document.getElementById("linha_10")
 
 let i_eu = document.getElementById("i_eu")
 
@@ -96,6 +109,12 @@ function modoClaro() {
     gc_github.style.color = "rgb(17, 38, 57)"
     gc_projeto.style.color = "rgb(17, 38, 57)"
 
+    jc_github.style.color = "rgb(17, 38, 57)"
+    jc_projeto.style.color = "rgb(17, 38, 57)"
+
+    pn_github.style.color = "rgb(17, 38, 57)"
+    pn_projeto.style.color = "rgb(17, 38, 57)"
+
     e_github.href = "https://www.github.com/marlonwillian"
     e_linkedin.href = "https://www.linkedin.com/in/marlon-willian-silva-barros-30b49122b/"
     e_insta.href = "https://www.instagram.com/mwbrrs/"
@@ -112,22 +131,32 @@ function modoClaro() {
 
     jogo_da_velha.style.color = "rgb(17, 38, 57)"
     gerador_de_cartas.style.color = "rgb(17, 38, 57)"
+    jogo_da_cobrinha.style.color = "rgb(17, 38, 57)"
+    pagina_netflix.style.color = "rgb(17, 38, 57)"
 
     jogo_da_velha.style.borderColor = "rgb(17, 38, 57)"
     gerador_de_cartas.style.borderColor = "rgb(17, 38, 57)"
+    jogo_da_cobrinha.style.borderColor = "rgb(17, 38, 57)"
+    pagina_netflix.style.borderColor = "rgb(17, 38, 57)"
 
     jogo_da_velha.style.backgroundColor = "rgb(209, 209, 209)"
     gerador_de_cartas.style.backgroundColor = "rgb(209, 209, 209)"
+    jogo_da_cobrinha.style.backgroundColor = "rgb(209, 209, 209)"
+    pagina_netflix.style.backgroundColor = "rgb(209, 209, 209)"
     
     hr3.style.backgroundColor = "rgb(17, 38, 57)"
     hr4.style.backgroundColor = "rgb(17, 38, 57)"
     hr5.style.backgroundColor = "rgb(17, 38, 57)"
     hr6.style.backgroundColor = "rgb(17, 38, 57)"
 
-    hr3.style.borderColor = "rgb(17, 38, 57)"
-    hr4.style.borderColor = "rgb(17, 38, 57)"
-    hr5.style.borderColor = "rgb(17, 38, 57)"
-    hr6.style.borderColor = "rgb(17, 38, 57)"
+    hr3.style.backgroundColor = "rgb(17, 38, 57)"
+    hr4.style.backgroundColor = "rgb(17, 38, 57)"
+    hr5.style.backgroundColor = "rgb(17, 38, 57)"
+    hr6.style.backgroundColor = "rgb(17, 38, 57)"
+    hr7.style.backgroundColor = "rgb(17, 38, 57)"
+    hr8.style.backgroundColor = "rgb(17, 38, 57)"
+    hr9.style.backgroundColor = "rgb(17, 38, 57)"
+    hr10.style.backgroundColor = "rgb(17, 38, 57)"
 
     i_eu.style.borderColor = "rgb(17, 38, 57)"
 
@@ -171,6 +200,12 @@ function modoEscuro() {
     gc_github.style.color = "white"
     gc_projeto.style.color = 'white'
 
+    jc_github.style.color = "white"
+    jc_projeto.style.color = "white"
+
+    pn_github.style.color = "white"
+    pn_projeto.style.color = "white"
+
     nome.style.color = "white"
     subtitulo.style.color = "white"
     tecnologias.style.color = "white"
@@ -192,15 +227,30 @@ function modoEscuro() {
     jogo_da_velha.style.backgroundColor = "rgb(17, 38, 57)"
     gerador_de_cartas.style.backgroundColor = "rgb(17, 38, 57)"
     
+    jogo_da_cobrinha.style.color = "white"
+    pagina_netflix.style.color = "white"
+
+    jogo_da_cobrinha.style.borderColor = "white"
+    pagina_netflix.style.borderColor = "white"
+
+    jogo_da_velha.style.backgroundColor = "rgb(17, 38, 57)"
+    gerador_de_cartas.style.backgroundColor = "rgb(17, 38, 57)"
+    jogo_da_cobrinha.style.backgroundColor = "rgb(17, 38, 57)"
+    pagina_netflix.style.backgroundColor = "rgb(17, 38, 57)"
+    
     hr3.style.backgroundColor = "white"
     hr4.style.backgroundColor = "white"
     hr5.style.backgroundColor = "white"
     hr6.style.backgroundColor = "white"
 
-    hr3.style.borderColor = "white"
-    hr4.style.borderColor = "white"
-    hr5.style.borderColor = "white"
-    hr6.style.borderColor = "white"
+    hr3.style.backgroundColor = "white"
+    hr4.style.backgroundColor = "white"
+    hr5.style.backgroundColor = "white"
+    hr6.style.backgroundColor = "white"
+    hr7.style.backgroundColor = "white"
+    hr8.style.backgroundColor = "white"
+    hr9.style.backgroundColor = "white"
+    hr10.style.backgroundColor = "white"
 
     i_eu.style.borderColor = "rgb(209, 209, 209)"
 
